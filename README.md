423baa06dbdaba1b63c1677ed83a2a4d991c107b
Sharon <khy869728@gmail.com>
2025-02-25 14:40:28 +0800

    新增 README.md 並更新 test1.sh(第二次)

---
+++ README.md
@@ -0,0 +1,2 @@
+# test-repo
+測試，包含sharon寫的  作業。
--- test1.sh
+++ test1.sh
@@ -3,15 +3,15 @@
 # ping $test1 -c 1 > test1.txt;
 # 清空txt
 > test1.txt
-username=`dialog --stdout --inputbox "你好!我想要統計大眾對於諾羅病毒的認知。現在要建立你的個人資料，請輸入你的名字(帳號):" 10 80`;
+username=`dialog --stdout --inputbox "您好!我想要統計大眾對於諾羅病毒的認知。\n現在要建立您的個人資料，請輸入你的名字(帳號):" 10 80`;
 # dialog --title "你輸入的帳號" --msgbox "$username" 10 80;
-passw=`dialog --stdout --passwordbox "請輸入你的密碼:" 10 80`;
-gender=`dialog --stdout --title "生理性別" --radiolist "單選" 10 30 2 1 "男" on 2 "女" off`;
-birth=`dialog --stdout --calendar "選擇你的生日" 15 50 9 6 1997`;
-echo "帳號: $username" >> test1.txt
-echo "密碼: $passw" >> test1.txt
-echo "性別: $gender" >> test1.txt
-echo "生日: $birth" >> test1.txt
+passw=`dialog --stdout --passwordbox "請輸入您的密碼:" 10 80`;
+gender=`dialog --stdout --title "您的生理性別" --radiolist "單選" 10 30 2 1 "男" on 2 "女" off`;
+birth=`dialog --stdout --calendar "請選擇生日" 15 50 9 6 1997`;
+echo "您的帳號: $username" >> test1.txt
+echo "您的密碼: $passw" >> test1.txt
+echo "您的性別: $gender" >> test1.txt
+echo "您的生日: $birth" >> test1.txt
 dialog --stdout --title "歡迎來到衛教小測驗(是非題)" --yesno "請問諾羅病毒用酒精消毒有用嗎?" 10 50;
 if [ $? -eq 0 ]; then
 	ans="Yes, 錯!酒精消毒沒用，請使用洗手乳或肥皂"
@@ -34,16 +34,15 @@ fi
 echo "問題:請問下列何者不是感染諾羅病毒的症狀? 1 嘔吐 2 腹瀉 3 發燒 4 以上皆是得到諾羅的症狀" >> test1.txt
 echo "回答: $ans2" >> test1.txt
 
-#question3=`dialog --stdout --title "問題3(複選題)" --checklist "請問感染諾羅病毒後，應採取甚麼行為?" 15 50 3 1 "由於此病原具有高度的傳播能力，儘量在家休息，不要去上學或上班。" on 2 "補充水分及電解質，避免脫水或電解質不平衡。" on 3 "生病期間患者最好不要為家人準備食物，建議在症狀消失至少2-3天後再開始。" off`
-#if [[ "$question3" == *"1"* || "$question3" == *"2"* || "$question3" == *"3"* ]]; then
-#	ans3="錯!感染諾羅後還有其他應採取的行為，正確答案為 1, 2, 3 皆是感染諾羅病毒後，應採取的行為"
-#elif [[ ( "$question3" == *"1"* && "$question3" == *"2"* ) || ( "$question3" == *"1"* && "$question3" == *"3"* ) || ( "$question3" == *"2"* && "$question3" == *"3"* ) ]]; then
-#	ans3="錯!感染諾羅後還有其他應採取的行為，正確答案為 1, 2, 3 皆是感染諾羅病毒後，應採取的行為"
-#else
-#	ans3="1, 2, 3 皆是感染諾羅病毒後，應採取的行為"
-#fi
+question3=`dialog --stdout --title "問題3(複選題)" --checklist "請問感染諾羅病毒後，應採取甚麼行為?" 15 50 3 1 "由於此病原具有高度的傳播能力，儘量在家休息，不要去上學或上班。" on 2 "補充水分及電解質，避免脫水或電解質不平衡。" on 3 "生病期間患者最好不要為家人準備食物，建議在症狀消失至少2-3天後再開始。" off`
+if [[ "$question3" == *"1"* || "$question3" == *"2"* || "$question3" == *"3"* ]]; then
+	ans3="錯!感染諾羅後還有其他應採取的行為，正確答案為 1, 2, 3 皆是感染諾羅病毒後，應採取的行為"
+elif [[ ( "$question3" == *"1"* && "$question3" == *"2"* ) || ( "$question3" == *"1"* && "$question3" == *"3"* ) || ( "$question3" == *"2"* && "$question3" == *"3"* ) ]]; then
+	ans3="錯!感染諾羅後還有其他應採取的行為，正確答案為 1, 2, 3 皆是感染諾羅病毒後，應採取的行為"
+else
+	ans3="1, 2, 3 皆是感染諾羅病毒後，應採取的行為"
+fi
 
-
-#echo "問題:請問感染諾羅病毒後，應採取甚麼行為?" >> test1.txt
-#echo "回答: $an3" >> test1.txt
-dialog --title "對答紀錄" --textbox ~/test1.txt 30 80
+echo "問題:請問感染諾羅病毒後，應採取甚麼行為?" >> test1.txt
+echo "回答: $ans3" >> test1.txt
+dialog --title "對答紀錄" --textbox ~/test1.txt 30 100
